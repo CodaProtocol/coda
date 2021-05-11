581a2a4a3f53875aca722c679c18b12e1baa07e2
umesh <umesh.bihani@bnt-soft.com>
2021-05-11 10:25:27 +0530

    updated config

--- automation/services/mina-bp-stats/payout-process/payouts_config.py
+++ automation/services/mina-bp-stats/payout-process/payouts_config.py
@@ -18,14 +18,13 @@ class BaseConfig(object):
     #POSTGRES_PAYOUT_DB = f'postgres://{POSTGRES_PAYOUT_USER}:{POSTGRES_PAYOUT_PASSWORD}@{POSTGRES_PAYOUT_HOST}:{POSTGRES_PAYOUT_PORT}/{POSTGRES_PAYOUT_DB}'
     COINBASE = 720
     SLOT_WINDOW_VALUE = 3500
-    CREDENTIAL_PATH = 'mina-mainnet-303900-45050a0ba37b.json'
-    API_KEY = 'AIzaSyA3z01DZpfHFDq5Ln1nnebOeJ2aXElvd1Y'
+    CREDENTIAL_PATH = ''
+    API_KEY = ''
     GCS_BUCKET_NAME = 'mina-staking-ledgers'
-    FROM_EMAIL = 'ruchika.jain@bnt-soft.com'
-    TO_EMAILS = ['umesh.bihani@bnt-soft.com', 'nilesh@bnt-soft.com', 'mehul.wankhede@bnt-soft.com']
+    FROM_EMAIL = ''
+    TO_EMAILS = ['']
     SUBJECT = 'LeaderBoard Stats As of{0}'.format(datetime.datetime.utcnow())
     PLAIN_TEXT = 'Report for Leaderboard as of {0}'.format(datetime.datetime.utcnow())
-    SENDGRID_API_KEY = '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'
+    SENDGRID_API_KEY = ''
     SPREADSHEET_SCOPE = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     SPREADSHEET_NAME = 'Mina Foundation Delegation Application (Responses)'
-
