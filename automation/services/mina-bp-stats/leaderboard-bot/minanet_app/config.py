7de8b123d2df3cf0b0b02a9b9fc9039d6cf80ed3
Mehul Wankhade <mehulwankhade@bnt-soft.com>
2021-06-29 15:12:28 +0530

    config changes done by mehul

--- automation/services/mina-bp-stats/leaderboard-bot/minanet_app/config.py
+++ automation/services/mina-bp-stats/leaderboard-bot/minanet_app/config.py
@@ -1,6 +1,6 @@
 import logging
 import datetime
-
+import os
 
 class BaseConfig(object):
     DEBUG = False
@@ -8,17 +8,17 @@ class BaseConfig(object):
     LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     LOGGING_LEVEL = logging.WARN
     LOGGING_LOCATION = 'minanet.log'
-    POSTGRES_HOST = '127.0.0.1'
-    POSTGRES_PORT = 5432
-    POSTGRES_USER = 'postgres'
-    POSTGRES_PASSWORD = 'postgres'
-    POSTGRES_DB = 'minanetdb'
+    POSTGRES_HOST = str(os.environ['POSTGRES_HOST']).strip()
+    POSTGRES_PORT = int(os.environ['POSTGRES_PORT'])
+    POSTGRES_USER = str(os.environ['POSTGRES_USER']).strip()
+    POSTGRES_PASSWORD = str(os.environ['POSTGRES_PASSWORD']).strip()
+    POSTGRES_DB = os.environ['POSTGRES_DB']
     SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
     CREDENTIAL_PATH = 'leaderboard-bot\minanet_app\mina-mainnet-303900-45050a0ba37b.json'
-    GCS_BUCKET_NAME = 'block-producer-stats'
+    GCS_BUCKET_NAME = str(os.environ['GCS_BUCKET_NAME']).strip()
     PROVIDER_ACCOUNT_PUB_KEYS_FILE = 'Mina_Foundation_Addresses.csv'
-    SURVEY_INTERVAL_MINUTES = 10 
-    UPTIME_DAYS_FOR_SCORE = 60
+    SURVEY_INTERVAL_MINUTES = int(os.environ['SURVEY_INTERVAL_MINUTES'])
+    UPTIME_DAYS_FOR_SCORE = int(os.environ['UPTIME_DAYS_FOR_SCORE'])
     FROM_EMAIL = ''
     TO_EMAILS = ['', '']
     SUBJECT = 'Ontab-key LeaderBoard positions As of {0}'.format(datetime.datetime.utcnow())
@@ -27,4 +27,4 @@ class BaseConfig(object):
     SPREADSHEET_SCOPE = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     SPREADSHEET_NAME = 'Mina Foundation Delegation Application (Responses)'
     SPREADSHEET_JSON = 'leaderboard-bot\minanet_app\mina-foundation-delegation-app-b889c28d5b9b.json'
-    MAX_THREADS_TO_DOWNLOAD_FILES=128
+    MAX_THREADS_TO_DOWNLOAD_FILES= int(os.environ["MAX_THREADS_TO_DOWNLOAD_FILES"])
