a2ab6ee10db0484d54d33d3d8155600f7241de2a
umesh <umesh.bihani@bnt-soft.com>
2021-07-16 07:48:49 +0530

    removed extra db commit - umesh

--- automation/services/mina-bp-stats/payout-process/main_app/payouts_config.py
+++ automation/services/mina-bp-stats/payout-process/main_app/payouts_config.py
@@ -1,45 +1,43 @@
 import datetime
 import logging
-import os
 
 
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
     LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    LOGGING_LEVEL = logging.WARN
+    LOGGING_LEVEL = logging.DEBUG
     LOGGING_LOCATION = 'minanet.log'
-    POSTGRES_ARCHIVE_HOST = str(os.environ['POSTGRES_ARCHIVE_HOST']).strip()
-    POSTGRES_ARCHIVE_PORT = int(os.environ['POSTGRES_ARCHIVE_PORT'])
-    POSTGRES_ARCHIVE_USER = str(os.environ['POSTGRES_ARCHIVE_USER']).strip()
-    POSTGRES_ARCHIVE_PASSWORD = str(os.environ['POSTGRES_ARCHIVE_PASSWORD']).strip()
-    POSTGRES_ARCHIVE_DB = str(os.environ['POSTGRES_ARCHIVE_DB']).strip()
+    POSTGRES_ARCHIVE_HOST = 'minanetdb.ctldjwfweirp.us-east-1.rds.amazonaws.com'
+    POSTGRES_ARCHIVE_PORT = 5432
+    POSTGRES_ARCHIVE_USER = 'mina_admin'
+    POSTGRES_ARCHIVE_PASSWORD = 'mina2021!'
+    POSTGRES_ARCHIVE_DB = 'archive'
 
-    POSTGRES_PAYOUT_HOST = str(os.environ['POSTGRES_PAYOUT_HOST']).strip()
-    POSTGRES_PAYOUT_PORT = int(os.environ['POSTGRES_PAYOUT_PORT'])
-    POSTGRES_PAYOUT_USER = str(os.environ['POSTGRES_PAYOUT_USER']).strip()
-    POSTGRES_PAYOUT_PASSWORD = str(os.environ['POSTGRES_PAYOUT_PASSWORD']).strip()
-    POSTGRES_PAYOUT_DB = str(os.environ['POSTGRES_PAYOUT_DB']).strip()
+    POSTGRES_PAYOUT_HOST = 'localhost'
+    POSTGRES_PAYOUT_PORT = 5432
+    POSTGRES_PAYOUT_USER = 'postgres'
+    POSTGRES_PAYOUT_PASSWORD = 'postgres'
+    POSTGRES_PAYOUT_DB = 'payout_o1labs'
 
-    POSTGRES_LEADERBOARD_HOST = str(os.environ['POSTGRES_LEADERBOARD_HOST']).strip()
-    POSTGRES_LEADERBOARD_PORT = int(os.environ['POSTGRES_LEADERBOARD_PORT'])
-    POSTGRES_LEADERBOARD_USER = str(os.environ['POSTGRES_LEADERBOARD_USER']).strip()
-    POSTGRES_LEADERBOARD_PASSWORD = str(os.environ['POSTGRES_LEADERBOARD_PASSWORD']).strip()
-    POSTGRES_LEADERBOARD_DB = str(os.environ['POSTGRES_LEADERBOARD_DB']).strip()
-
-    COINBASE = int(os.environ['COINBASE'])
-    SLOT_WINDOW_VALUE = int(os.environ['SLOT_WINDOW_VALUE'])
-    CREDENTIAL_PATH = str(os.environ['CREDENTIAL_PATH']).strip()
-    API_KEY = str(os.environ['API_KEY']).strip()
-    OVERRIDE_EMAIL = os.environ['OVERRIDE_EMAIL']
-    GCS_BUCKET_NAME = str(os.environ['GCS_BUCKET_NAME']).strip()
-    FROM_EMAIL = str(os.environ['FROM_EMAIL'])
-    PROVIDER_EMAIL = os.environ['PROVIDER_EMAIL'].split(',')
-    TO_EMAILS = os.environ['TO_EMAILS'].split(',')
+    POSTGRES_LEADERBOARD_HOST = 'minanetdb.ctldjwfweirp.us-east-1.rds.amazonaws.com'
+    POSTGRES_LEADERBOARD_PORT = 5432
+    POSTGRES_LEADERBOARD_USER = 'mina_admin'
+    POSTGRES_LEADERBOARD_PASSWORD = 'mina2021!'
+    POSTGRES_LEADERBOARD_DB = 'leaderboard'
+    COINBASE = 720
+    SLOT_WINDOW_VALUE = 3500
+    CREDENTIAL_PATH = 'mina-mainnet-303900-45050a0ba37b.json'
+    API_KEY = ''
+    GCS_BUCKET_NAME = 'mina-staking-ledgers'
+    FROM_EMAIL = 'mehul.wankhede@bnt-soft.com'
+    OVERRIDE_EMAIL='umesh@ontab.com'
+    PROVIDER_EMAIL = ['umesh@ontab.com']
+    TO_EMAILS = ['umesh@ontab.com']
     SUBJECT = 'LeaderBoard Stats As of{0}'.format(datetime.datetime.utcnow())
     PLAIN_TEXT = 'Report for Leaderboard as of {0}'.format(datetime.datetime.utcnow())
-    SENDGRID_API_KEY = str(os.environ['SENDGRID_API_KEY']).strip()
-    DELEGATION_ADDRESSS_CSV = str(os.environ['DELEGATION_ADDRESSS_CSV']).strip()
+    SENDGRID_API_KEY = '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'
     SPREADSHEET_SCOPE = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    SPREADSHEET_NAME = str(os.environ['SPREADSHEET_NAME']).strip()
+    SPREADSHEET_NAME = 'Mina Foundation Delegation Application (Responses)'
+    DELEGATION_ADDRESSS_CSV='O1_Labs_addresses_1.csv'
     GENESIS_DATE = datetime.datetime(2021, 3, 17)
